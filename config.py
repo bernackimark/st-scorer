0ca3c962d44e8cffe4776871f36d7d943da4ded5
bernacki <bernackimark@gmail.com>
2025-02-13 14:37:01 -0500

    added adjectives & animals

---
+++ config.py
@@ -0,0 +1,18 @@
+BASE_URL = 'https://st-scorer.streamlit.io'
+
+
+ADJECTIVES = (
+    "bright", "clever", "gentle", "jolly", "brave", "quirky", "sunny", "zesty",
+    "mellow", "bouncy", "cranky", "eager", "glossy", "hushed",
+    "lively", "mystic", "narrow", "plucky", "quirky", "robust", "snappy", "thrifty",
+    "unique", "vibrant", "witty", "daring", "frothy", "hearty", "lucid",
+    "nimble", "quirky", "zesty"
+)
+
+ANIMALS = (
+    "badger", "bison", "bobcat", "canary", "cougar", "donkey", "ferret",
+    "gopher", "hornet", "iguana", "jackal", "jaguar", "koala", "lemur", "lizard",
+    "magpie", "monkey", "otter", "panda", "parrot", "rabbit",
+    "raven", "salmon", "skunk", "squid", "turtle", "urchin", "viper",
+    "weasel", "wombat", "zebra"
+)
